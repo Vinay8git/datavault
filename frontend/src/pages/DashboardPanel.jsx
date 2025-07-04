843d6d3e3c17def01ea8ef426b412c552ef04a6e
VibhorSharma0053 <144813390+VibhorSharma0053@users.noreply.github.com>
2025-07-04 22:44:29 +0530

    Pagination category wise

--- frontend/src/pages/DashboardPanel.jsx
+++ frontend/src/pages/DashboardPanel.jsx
@@ -1,10 +1,10 @@
 import RecentFiles from "../components/RecentFiles.jsx";
 import Overview from "./Overview.jsx";
 
-const DashboardPanel = () => {
+const DashboardPanel = (props) => {
   return (
     <div className=" w-full h-full flex items-center justify-center gap-10">
-      <Overview />
+      <Overview  data={props.data} />
       <RecentFiles />
     </div>
   );
--- frontend/src/pages/Overview.jsx
+++ frontend/src/pages/Overview.jsx
@@ -1,15 +1,19 @@
 import StorageCategoryCard from "../components/StorageCategoryCard.jsx";
 import StorageCard from "../components/UI/StorageCard.jsx";
+import calculateStorage from "../components/Utility/calculateStorage.jsx";
 
-const Overview = () => {
+const Overview = (props) => {
+  const result = calculateStorage(props.data);
+  console.log(result.used);
+
   return (
     <div className="w-[40%] h-[100%] bg-purple-200 flex flex-col justify-around items-center rounded-2xl px-1">
-      <StorageCard />
+      <StorageCard used={result.used} />
       <div className="flex gap-6 items-center justify-center flex-wrap">
-        <StorageCategoryCard type="document" storage="20 GB" title="Document" lastUpdate="10:15am, 10 Oct" />
-        <StorageCategoryCard type="image" storage="10 GB" title="Images" lastUpdate="10:45am, 18 Sep"  />
-        <StorageCategoryCard type="media" storage="30 GB" title="Videos" lastUpdate="10:15am, 10 Oct"  />
-        <StorageCategoryCard type="other" storage="40 GB" title="Others" lastUpdate="10:15am, 10 Oct"  />
+        <StorageCategoryCard type="document" storage={result.document} title="Document" lastUpdate="10:15am, 10 Oct" />
+        <StorageCategoryCard type="image" storage={result.image} title="Images" lastUpdate="10:45am, 18 Sep"  />
+        <StorageCategoryCard type="media" storage={result.media} title="Videos" lastUpdate="10:15am, 10 Oct"  />
+        <StorageCategoryCard type="other" storage={result.other} title="Others" lastUpdate="10:15am, 10 Oct"  />
       </div>
     </div>
   );
